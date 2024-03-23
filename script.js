535a7f9c3ace3822112f70078dbd0538dda3157a
Methuselah <dstengac@gmail.com>
2024-03-23 18:47:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("hello");
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+darkModeToggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+// Сохранение состояния переключателя
+window.addEventListener("load", () => {
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+  if (isDarkMode) {
+    darkModeToggle.checked = true;
+    document.body.classList.add("dark-mode");
+  }
+});
+
+darkModeToggle.addEventListener("change", () => {
+  const isDarkMode = darkModeToggle.checked;
+  localStorage.setItem("darkMode", isDarkMode);
+});
+
+// ----------------------------------------- ScrollReveal ------------------------------
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal("header", { delay: 100 });
+sr.reveal(".baner__img", { delay: 100 });
+sr.reveal(".baner__text", { delay: 500, interval: 100, origin: "bottom" });
+sr.reveal(".statistics", { delay: 100 });
+sr.reveal(".statistics__list", { delay: 200 });
+sr.reveal(".section", { delay: 100, interval: 100 });
